0eb9571b4bc5a8d299867b9948ec99f80ec8446f
erickmattoso <mattosoerick@gmail.com>
2022-02-03 16:33:53 +0100

    update

---
+++ scripts/index.sh
@@ -0,0 +1,13 @@
+python 01-get_data.py
+python 02-coordinates.py
+python 03-calculation.py
+python 04-get_dates.py
+python 05-cost_living.py
+git add .
+git commit -m "update"
+git push
+cd ..
+cd app
+git add .
+git commit -m "update"
+git push
