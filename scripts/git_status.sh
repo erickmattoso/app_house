028cf3c9b46401c9a515ca8b98ad1f4256d95c03
Ericson Marques <ericsonmarques@ericsons-mbp.kpn>
2022-02-03 15:38:06 +0100

    update

---
+++ scripts/git_status.sh
@@ -0,0 +1,5 @@
+cd ..
+cd app
+git add .
+git commit -m "update"
+git push
---
+++ scripts/main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python 01-get_data
+python 02-coordinates
+python 03-calculation
+python 04-get_dates
+python 05-cost_living
