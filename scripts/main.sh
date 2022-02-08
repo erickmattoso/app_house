2d31cf2034cd6ef872d7f475cfebff98a7d3ff62
erickmattoso <mattosoerick@gmail.com>
2022-02-08 14:26:59 +0100

    update

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,13 +1,2 @@
-python 01-get_data.py
-python 02-coordinates.py
-python 03-calculation.py
-python 04-get_dates.py
-python 05-cost_living.py
-git add .
-git commit -m "update"
-git push
-cd ..
-cd app
-git add .
-git commit -m "update"
-git push
+Get-Date | Out-File E:\Demo\log4video.txt -Append
+Get-PSDrive -PSProvider FileSystem | Out-File E:\Demo\log4video.txt -Append
